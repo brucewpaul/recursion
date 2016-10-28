330bd414f3af0762497bd2ddaab39ca7abad7ce9
Bruce Paul <brucewpaul@gmail.com>
2016-10-28 08:31:10 -0700

    Implement getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,23 +1,34 @@
 var getElementsByClassName = function(className) {
 
+  // setup element to capture the elements that pass test
   var elementsWithClass = []
 
+  // create functoin to check DOM elements to be called recursively
   function checkDOM(el){
 
     console.log(el.childNodes,el.classList)
 
-    if(el.classList.contains(className)){
-      elementsWithClass.push(el)
+    // check if the classList for the provided node is undefined
+    if(el.classList !== undefined){
+      // check if the classList contains the className we are looking for
+      if(el.classList.contains(className)){
+        // add element to our array
+        elementsWithClass.push(el)
+      }
     }
 
+    // foreach childNode of given element, recursively call checkDOM
+    // so that we check all elements in the vody
     for(var i=0;i<el.childNodes.length;i++){
       checkDOM(el.childNodes[i])
     }
 
   }
 
+  // initially call checkDOM with the document.body
   checkDOM(document.body)
 
+  // finally return our array of elements
   return elementsWithClass
 
 };
