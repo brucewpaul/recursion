aad1597a8c3fee154d269c1089449fbdce221222
Bruce Paul <brucewpaul@gmail.com>
2016-10-27 21:01:49 -0700

    Implementation for number, boolean, string, null

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,27 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if( obj == null ){
+
+    return "null"
+
+  } else if( typeof obj == 'object') {
+
+    if( Array.isArray(obj) ){
+      return '['+obj.toString()+']'
+    }
+
+  } else if( typeof obj == 'number' ){
+
+    return obj.toString()
+
+  } else if( typeof obj == 'boolean') {
+
+    return obj.toString()
+
+  } else if( typeof obj == 'string') {
+
+    return '"'+obj+'"'
+
+  }
+
 };
